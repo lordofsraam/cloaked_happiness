de2481c35da69ed46cdef365ee00b841a56f56c3
lordofsraam <lordofsraam@hotmail.com>
2013-12-24 10:38:38 -0800

    Made the randomness weighted

--- markov.py
+++ markov.py
@@ -24,19 +24,27 @@ for letter in lang_signature:
 				else: lang_signature[letter][w[i+2]] = 1
 
 def genWord(c='a',l=5):
+	c = c.lower()
 	org = c
 	ii = 0
 	while ii < len(c):
 		if c[ii::] in lang_signature:
 			c = c[ii::]
 			r_str = c
-			print "Using "+c
 			break
 		else:
 			ii += 1
 
 	for i in xrange(l-len(c)):
-		_c = sorted(lang_signature[c].iteritems(),key=operator.itemgetter(1),reverse=True)[random.randint(0,(len(lang_signature[c])/6))][0]
+		letter_signature = lang_signature[c]
+		sorted_signature = sorted(lang_signature[c].iteritems(),key=operator.itemgetter(1),reverse=True)
+		weighted_list = []
+		total = 0
+		for j in xrange(len(lang_signature[c])/2):
+			total += sorted_signature[j][1]
+		for j in xrange(len(lang_signature[c])/2):
+			weighted_list += [sorted_signature[j][0]]*int((float(sorted_signature[j][1])/total)*100)
+		_c = random.choice(weighted_list)
 		r_str += _c
 		c = _c
 	return org[:ii:]+r_str
