9b7e3efd80c1808ae96b9ba0641439f58e95d496
lordofsraam <lordofsraam@hotmail.com>
2019-02-20 16:45:32 -0500

    Update youtube_suggested.py

--- youtube_suggested.py
+++ youtube_suggested.py
@@ -2,10 +2,11 @@ import urllib2
 from bs4 import BeautifulSoup
 import operator
 
+
 class Link(object):
     def __init__(self, title, href):
         self.title = title.strip()
-        self.url = 'https://www.youtube.com'+href
+        self.url = 'https://www.youtube.com' + href
 
     def __str__(self):
         return self.title
@@ -20,10 +21,22 @@ class Page(object):
             vid_page_src = urllib2.urlopen(link)
             self.url = link
 
+        if vid_page_src is None:
+            raise ValueError('Failed to load ' + self.url)
+
         self.page = BeautifulSoup(vid_page_src, 'html.parser')
-        res = self.page.find_all('a', 
-                        attrs={'class': ' content-link spf-link yt-uix-sessionlink spf-link '})
-        
+
+        res = self.page.find_all('a',
+                                 attrs={'class': ' content-link spf-link yt-uix-sessionlink spf-link '})
+        if len(res) == 0:
+            # Sometimes the html class doesnt have spaces and bs4 doesn't strip
+            #    so we gotta look again
+            res = self.page.find_all('a',
+                                     attrs={'class': 'content-link spf-link yt-uix-sessionlink spf-link'})
+
+        if len(res) == 0:
+            raise ValueError('No suggested links found for ' + self.url)
+
         self.title = self.page.title.text
         self.suggested = []
         for s in res[:3]:
@@ -32,27 +45,28 @@ class Page(object):
     def __str__(self):
         return self.title
 
-    def recur(self, exclude=None):
+    def recur(self):
         if Page in map(type, self.suggested):
             return
-        
+
         for i in range(len(self.suggested)):
-            print '\tPaging', i+1, 'out of', len(self.suggested), '...',
+            print '\tPaging', i + 1, 'out of', len(self.suggested), '...',
             self.suggested[i] = Page(self.suggested[i])
             print 'Done'
 
     def contains(self, link):
         if type(link) == Link:
             link = link.url
-        
+
         for s in self.suggested:
             if s.url == link:
                 return True
-        
+
         return False
 
+
 db = {}
-vid_page_url = 'https://www.youtube.com/watch?v=0sqhSybK0P0'
+vid_page_url = 'https://www.youtube.com/watch?v=dBbKtOvfTiE'
 
 print 'Getting page from', vid_page_url, '...',
 
@@ -71,15 +85,15 @@ level = [p]
 for i in range(3):
     for page in level:
         page.recur()
-    print 'Recur lvl', i+1, 'complete'
+    print 'Recur lvl', i + 1, 'complete'
     level = [item for sublist in map(lambda x: x.suggested, level) for item in sublist]
-    #map(lamda x: db[x.title] = db.setdefault(x.title, 0) + 1, level)
+    # map(lambda x: db[x.title] = db.setdefault(x.title, 0) + 1, level)
     for l in level:
         db[l.title] = db.setdefault(l.title, 0) + 1
 
- most_common = max(db.iteritems(), key=operator.itemgetter(1))[0]
+most_common = max(db.iteritems(), key=operator.itemgetter(1))[0]
 
- print 'Most common video:'
- print most_common
+print 'Most common video:'
+print most_common
 
- counts = db.values()
+counts = db.values()
