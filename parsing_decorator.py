6470ffc2bb429eba40a2501bfb7027cffda7300d
lordofsraam <lordofsraam@hotmail.com>
2021-05-09 16:01:14 -0400

    Update parsing_decorator.py

--- parsing_decorator.py
+++ parsing_decorator.py
@@ -1,44 +1,7 @@
+import ast
 import functools
+import shlex
 from inspect import signature
-
-"""
-TODO: Instead of tokenizing the string (leads to major issues), should make the parsers accept 
-the entire string, "consume" only what they need, and return the new truncated string that will 
-be passed along to the next parameter.
-"""
-
-class DiceRollParser(object):
-    def __call__(self, token: str) -> int:
-        magic_goop = r"(\d+d\d+)"
-        match = re.search(magic_goop, token)
-        
-        if not match:
-            raise ValueError('DiceRolls should be formatted as [die amount]d[dice faces]')
-
-        roll_split = match.groups()[0].split('d')
-
-        die_amount = int(roll_split[0])
-        dice_faces = int(roll_split[1])
-
-        if die_amount > 20 or dice_faces > 100:
-            raise ValueError('DiceRolls does not accept numbers that high')
-
-        total = 0
-
-        for _ in range(die_amount):
-            total += random.randint(1, dice_faces)
-
-        return total
-
-
-class StatParser(object):
-    def __call__(self, token: str) -> str:
-        token = token.lower()
-
-        if token not in player.valid_stat_names:
-            raise ValueError(f'Stats should be one of: {player.valid_stat_names}')
-
-        return token
 
 
 class BoolParser(object):
@@ -63,12 +26,14 @@ class BoolParser(object):
 """
 Example usage:
 
-@require_args(str, int, BoolParser()):
+@require_args(str, int, BoolParser())
 def foo(some_str, some_int, some_bool):
   pass
 
+"\tone two three" 4 false
+
 """
-      
+
 def require_args(*args):
     """ Require these arguments. They will be passed to the function as the types provided inside the kwarg 'parsed_args'. """
     
@@ -77,33 +42,84 @@ def require_args(*args):
     # we can't rely on dictionaries to be in order (even in Python3)
     def require_arg(func):
         sig = signature(func)
-        needed_parameters = len(args) + 2  # Magic number. Anticipates base command trigger word?
+
+        self_ref_count = 0
+        if '.' in func.__qualname__:
+            self_ref_count = 1
+
+        needed_parameters = len(args) + 1 + self_ref_count # +1 to fit the original string
         actual_parameters = len(sig.parameters)
         if actual_parameters < needed_parameters:
             raise AttributeError(f'Decorated function {func} does not accept the correct about of parameters. Needed {needed_parameters}, actual {actual_parameters}')
 
-        @functools.wraps(func)
-        def wrapper(self, message):
-            # Get the actual and required parameter counts
-            tokens = message.content.split()
-            require_count = len(args)
+        if self_ref_count:
+            @functools.wraps(func)
+            def wrapper(self, message):
+                # Get the actual and required parameter counts
+                # Hack to get input() to allow escape characters
+                tokens = shlex.split(str(ast.literal_eval(shlex.quote(message))))
+                # If the input is coming from code (ie. literal strings) use the following line instead
+                # tokens = shlex.split(message)
+                require_count = len(args) - self_ref_count
 
-            # Send error message if counts dont match
-            if len(tokens) - 1 < require_count:
-                print(f'Insufficient arguments to command. {require_count} needed.')
-                return
-
-            # Parse only as many tokens as requested as requested type
-            parsed_args = []
-            for i, arg in enumerate(tokens[1:needed_parameters-1]):
-                try:
-                    parsed_args.append(args[i](arg))
-                except Exception as e:
-                    print(f'Error in parsing argument "*{arg}*" as a "*{args[i]}*". Error:\n{e}')
+                # Send error message if counts dont match
+                if len(tokens) < require_count:
+                    print(f'Insufficient arguments to command. {require_count} needed.')
                     return
-            
-            func(self, message, *parsed_args)
 
-        return wrapper
+                # Parse only as many tokens as requested as requested type
+                parsed_args = []
+                for i, arg in enumerate(tokens):
+                    try:
+                        parsed_args.append(args[i](arg))
+                    except Exception as e:
+                        print(f'Error in parsing argument "*{arg}*" as a "*{args[i]}*". Error:\n{e}')
+                        return
+                
+                func(self, message, *parsed_args)
+
+            return wrapper
+        else:
+            @functools.wraps(func)
+            def wrapper(message):
+                # Get the actual and required parameter counts
+                tokens = shlex.split(str(ast.literal_eval(shlex.quote(message))))
+                # tokens = shlex.split(message)
+                require_count = len(args)
+
+                # Send error message if counts dont match
+                if len(tokens) < require_count:
+                    print(f'Insufficient arguments to command. {require_count} needed.')
+                    return
+
+                # Parse only as many tokens as requested as requested type
+                parsed_args = []
+                for i, arg in enumerate(tokens):
+                    try:
+                        parsed_args.append(args[i](arg))
+                    except Exception as e:
+                        print(f'Error in parsing argument "*{arg}*" as a "*{args[i]}*". Error:\n{e}')
+                        return
+                
+                func(message, *parsed_args)
+
+            return wrapper
     
     return require_arg
+
+if __name__ == "__main__":
+
+    class A:
+        @require_args(int)
+        def bar(self, ogs: str, i: int):
+            print(f'Input: "{ogs}"')
+            print('Parsed:', i)
+    
+    @require_args(str, int, BoolParser())
+    def foo(original_str: str, some_str: str, some_int: int, some_bool: bool):
+        print(f'Input: "{original_str}"')
+        print('Parsed:', some_str, some_int, some_bool)
+
+    a = A()
+    foo(input('function-> '))
+    a.bar(input('method-> '))
